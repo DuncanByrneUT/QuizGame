da76da25127c237f0d84134ef45f8cf9c2d9b225
DuncanByrneUT <duncanbyrne7254@gmail.com>
2024-12-22 22:28:30 -0500

    added .lower to allow all answers to be converted to lowercase regardless if the user used upper or lowercase

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ playing = input("Do you want to play? ")
 # after intro
 
 # if user says anything other than yes, the code stops running
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Okay!! Let's play!")
@@ -14,28 +14,28 @@ print("Okay!! Let's play!")
 # first question
 
 answer = input("What does CPU stand for? ")
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print("Correct!")
 else:
     print("Incorrect! :(")
 
 # <---- second question
 answer = input("What does GPU stand for? ")
-if answer == "graphics processing unit":
+if answer.lower() == "graphics processing unit":
     print("Correct!")
 else:
     print("Incorrect! :(")
 
 # <---- third question
 answer = input("What does RAM stand for? ")
-if answer == "random access memory":
+if answer.lower() == "random access memory":
     print("Correct!")
 else:
     print("Incorrect! :(")
 
 # <---- fourth question
 answer = input("What does PSU stand for? ")
-if answer == "power supply unit":
+if answer.lower() == "power supply unit":
     print("Correct!")
 else:
     print("Incorrect! :(")
